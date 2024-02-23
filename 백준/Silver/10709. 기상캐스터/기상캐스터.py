048c2f3effc83cf4b9b4c01f74305fa255c09d66
이도형 <91038179+leedohyung28@users.noreply.github.com>
2024-02-23 18:31:27 +0900

    [Silver V] Title: 기상캐스터, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.py
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.py
@@ -1,6 +1,5 @@
 import sys
 
-# 입력 받기
 H, W = map(int, sys.stdin.readline().split())
 
 cloud_map = []
@@ -9,20 +8,19 @@ for _ in range(H) :
     cloud_map.append(list(sys.stdin.readline().rstrip()))
 
 
-# 순서대로 출력
 for i in range(H) :
-    time = 0                # 구름이 오는 시간 (기본값 0)
-    cloud_coming = False    # 구름 접근 여부 (기본값 False)
+    time = 0
+    cloud_coming = False
     for k in range(W) :        
-        if cloud_map[i][k] == 'c' : # 구름이라면
-            time = 0                # 구름이 오는 시간 0으로 초기화
-            print(time, end=' ')    # 출력
-            cloud_coming = True     # 구름 접근 여부 -> True
+        if cloud_map[i][k] == 'c' :
+            time = 0
+            print(time, end=' ')
+            cloud_coming = True
         
-        elif cloud_coming is True : # 구름이 접근 중이라면
-            time += 1               # 접근 시간 + 1
-            print(time, end=' ')    # 출력
+        elif cloud_coming is True :
+            time += 1
+            print(time, end=' ')
         
-        else :                      # 구름 X & 구름 접근 중 X
-            print(-1, end=' ')      # 출력
-    print() # 줄바꿈
+        else :
+            print(-1, end=' ')
+    print()
