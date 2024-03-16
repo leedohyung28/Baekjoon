17e6a546e727bed6cf44a9445bcaf1679ba7755a
이도형 <91038179+leedohyung28@users.noreply.github.com>
2024-03-16 14:49:30 +0900

    [Silver I] Title: 민겸 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/21314. 민겸 수/민겸 수.py
@@ -0,0 +1,37 @@
+import sys
+
+input = list(sys.stdin.readline().strip())
+
+most_num = ''
+least_num = ''
+
+M_most = 0
+M_least = False
+for i in input :
+    if i == 'M' :
+        
+        if not M_least :
+            M_least = True
+            least_num += '1'
+        else :
+            least_num += '0'
+        
+        M_most += 1
+        
+    else :
+        M_least = False
+        least_num += '5'
+        
+        if M_most == 0 :
+            most_num += '5'
+        else :
+            most_num += '5'
+            most_num += '0' * M_most
+            M_most = 0
+    
+if M_most != 0 :
+    most_num += '1' * (M_most)
+    # most_num += '0' * (M_most-1)
+            
+print(most_num)
+print(least_num)
